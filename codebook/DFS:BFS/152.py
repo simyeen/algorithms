6de36352d4a61be817ac024c3297f68746a212a0
simyeen <simyeen@naver.com>
2021-03-30 23:53:01 +0900

    152.py

--- codebook/DFS:BFS/152.py
+++ codebook/DFS:BFS/152.py
@@ -6,24 +6,27 @@ arr = []
 for _ in range (row) :
     arr.append(list(map(int, input())))
 
+drow = [-1, 0, 1, 0] #반시계 방향 up left down right 순이다.
+dcol = [0, -1, 0, 1]
+
 dist = 1
 
-def bfs(x,y):
-    queue = deque([x, y, dist])
-    print("before row, col, dist = ", x, y, dist)
-    
-    while queue :
-        v = queue.popleft()
-        
-        if not arr[x-1][y] == 0 :
-            queue.append([x-1, y, dist+1])
-        if not arr[x+1][y] == 0 :
-            queue.append([x+1, y, dist+1])
-        if not arr[x][y-1] == 0 :
-            queue.append([x, y-1, dist+1])
-        if not arr[x][y+1] == 0 :
-            queue.append([x, y+1, dist+1])
-            
+def bfs(r,c):
+    queue = deque()
+    queue.append((r,c))
 
-bfs(1,1)
-print(cnt)
+    while queue :
+        r, c = queue.popleft()
+        for i in range(4) :
+            nr = r + drow[i]
+            nc = c + dcol[i]
+            if nr < 0 or nc < 0 or nr >= row or nc >= col :
+                continue
+            if arr[nr][nc] == 0 :
+                continue
+            if arr[nr][nc] == 1 :
+                arr[nr][nc] = arr[r][c] + 1
+                queue.append((nr,nc))
+    return arr[row-1][col-1]
+
+print(bfs(0,0))
