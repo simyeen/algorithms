93d1a50ab8267408250d3673cb350d048e8f2b14
simyeen <simyeen@naver.com>
2021-03-31 16:32:10 +0900

    bSearchBaseRecur

---
+++ codebook/search/binSearch_recur.py
@@ -0,0 +1,20 @@
+def binSearch(arr, target, start, end) :
+    if start > end :
+        return None
+
+    mid = (start + end)//2
+    if arr[mid] == target :
+        return mid # index를 return한다.
+    if arr[mid] > target :
+        return binSearch(arr, target, start, mid-1)
+    elif arr[mid] < target :
+        return binSearch(arr, target, mid+1, end)
+        
+n, target = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+
+result = binSearch(arr, target, 0, n-1)
+if result == None:
+    print('no item')
+else :
+    print(result + 1)
