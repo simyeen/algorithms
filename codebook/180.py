938e91b07fd1ab7c4d0814017a8102e97bcbe2d6
simyeen <simyeen@naver.com>
2021-03-31 15:37:04 +0900

    180.py

---
+++ codebook/180.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+arr = []
+for i in range(n) :
+    arr.append(input().split())
+
+def setting(data):
+    return int(data[1])
+
+result = sorted(arr, key = setting)
+
+ans = []
+for i in range(n) :
+    ans.append(result[i][0])
+print(*ans)
