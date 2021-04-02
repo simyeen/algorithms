6245b2ad29bd034344e2fe2be609f6dc5494da04
simyeen <simyeen@naver.com>
2021-04-02 15:36:07 +0900

    223.py

---
+++ codebook/DP/226.py
@@ -0,0 +1,21 @@
+n, m  = map(int, input().split())
+
+d = [-1]*(m+1)
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
+    d[arr[i]] = 1    
+
+
+for i in range(1, m+1):
+    temp = []
+
+    for j in arr:
+        if d[i] == -1 and i > j:
+            temp.append(d[i-j]) 
+    print(temp)
+    d[i] = min(temp)+1
+    
+
+print(arr, d)
