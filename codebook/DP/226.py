493e57a67d327e3d64c36bd6ef8bc9e6b2b4ce4a
simyeen <simyeen@naver.com>
2021-04-06 15:43:10 +0900

    226.py

--- codebook/DP/226.py
+++ codebook/DP/226.py
@@ -1,24 +1,22 @@
 n, m  = map(int, input().split())
 
-d = [-1]*(m+1)
+d = [-1]*(m+2)
 arr = []
 
 for i in range(n):
     arr.append(int(input()))
     d[arr[i]] = 1    
 
-for i in range(1, m+1):
-    temp = []
-
-    for j in arr:
-        if d[i] == -1 and i > j:
-            temp.append(d[i-j]) 
-    print(temp)
-    d[i] = min(temp)+1
+for i in range(arr[0], m+1):
+    if i in arr :
+        continue
+    min_value = 9999
+    for j in arr :
+        if d[i-j] != -1 :
+          min_value = min(min_value, d[i-j])+1
+    d[i] = min_value
     
-
-print(arr, d)
-
+print(d[m])
 
 
 
