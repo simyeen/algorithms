cf74e8e9033b348f41303605a20ee11d3a9743c2
simyeen <simyeen@naver.com>
2021-04-13 21:47:24 +0900

    mst

---
+++ codebook/graph/300.py
@@ -0,0 +1,36 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent,parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = [i for i in range(n+1)]
+edges = []
+result = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+
+cnt = 0 
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+    elif find_parent(parent, a) == find_parent(parent, b): 
+        continue
+
+max_cost = edges[-1][0]
+
+print(result-cost)
