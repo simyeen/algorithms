a6e01d149d14f4b86ac1a46020c4f5206d95e7d1
simyeen <simyeen@naver.com>
2021-04-17 16:09:53 +0900

    comment

--- codebook/greedy/312_sol.py
+++ codebook/greedy/312_sol.py
@@ -1,7 +1,7 @@
 data = input()
 
 result = int(data[0])
-for i in data[1:]:
+for i in data[1:]: # same that using range(1, len(data)) but you have to access num = int(data[i]) 
     now = int(i)
     if result <= 1 or now <= 1:
         result += now
