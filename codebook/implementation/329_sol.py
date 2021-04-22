6c44f5b5a69a46e7e47a1aa77ab462e895e75aa3
simyeen <simyeen@naver.com>
2021-04-22 16:04:51 +0900

    329_sol.py

---
+++ codebook/implementation/329_sol.py
@@ -0,0 +1,28 @@
+
+def possible(answer):
+    for x, y, stuff in answer:
+        if stuff == 0: # 설치된 것이 기둥인 경우
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer :
+                continue
+            return False
+        elif stuff == 1:
+            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
+                continue
+            return False
+        return True
+
+def solution(n, build_frame):
+    answer = []
+    for frame in build_frame:
+        x, y, stuff, operator = frame
+        if operator == 0:
+            answer.remove([x, y, stuff])
+            if not possible(answer):
+                answer.append([x, y, stuff])
+        if operator == 1:
+            answer.append([x, y, stuff])
+            if not possible(answer):
+                answer.remove([x, y, stuff])
+    return sorted(answer)
+
+
