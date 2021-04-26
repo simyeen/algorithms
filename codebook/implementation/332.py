6bd96676af860d55fe1052bf5af17939f9c2e5a1
simyeen <simyeen@naver.com>
2021-04-26 22:47:50 +0900

    332_py retry

--- codebook/implementation/332.py
+++ codebook/implementation/332.py
@@ -1,8 +1,7 @@
-import heapq
 import sys
 from itertools import combinations
 
-# input = sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
@@ -12,36 +11,35 @@ for i in range(n):
 
 houses = []
 chickens = []
-distances = [[] for i in range(n+1)] 
-answer = 0
+select_list = []
 
-for i in range (n):
-    for j in range (n) :
-        if arr[i][j] == 1: 
-            houses.append((i,j))
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] == 1:
+            houses.append((i, j))
         elif arr[i][j] == 2:
-            chickens.append((i,j))
+            chickens.append((i, j))
+select_list = list(combinations(chickens,m))
 
-def get_distance():
-    distance = 0
-    for i in range (len(houses)):
-        for j in range(len(chickens)):       
-            distance = abs(houses[i][0] - chickens[j][0]) + abs(houses[i][1] - chickens[j][1])
-            heapq.heappush(distances[i], (i, distance))
-get_distance()
+distance = [9999 for _ in range(len(houses))]
 
-aa = list(combinations(distances, 2))
-print(aa)
+def get_distance(x, y):
 
-result = []
-temp = 0
-print(distances)
-for _ in range(m):
-    for i in range(len(houses)):
-        now, distance = heapq.heappop(distances[i])
-        temp += distance
-        print(distance, temp)
-    result.append(temp)
+    diff = x - y
+    if diff < 0:
+        return -diff
+    else:
+        return diff
 
-print(result)
-print(min(result))
+for select in select_list:
+    for i in range(len(select)):
+        for j in range(len(houses)):
+            target = get_distance(select[i][0], houses[j][0]) + get_distance(select[i][1], houses[j][1])
+            if target < distance[j]:
+                distance[j] = target
+            print(distance)
+print(sum(distance))
+
+
+
+
