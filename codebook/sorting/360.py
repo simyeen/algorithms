fae7d3de1ff75f812016528268daa1f14a56ea5d
simyeen <simyeen@naver.com>
2021-05-19 17:24:53 +0900

    retry+360

--- codebook/sorting/360.py
+++ codebook/sorting/360.py
@@ -3,16 +3,22 @@ n = int(input())
 house = list(map(int, input().split()))
 house.sort()
 
+count = []
+
 start = house[0]
 last = house[-1]
+
+for i in house:
+    count[i] += 1
 
 min_value = 1e9
 answer = start
 
+ho = set(house)
 for i in range(start, last+1):
     result = 0
-    for h in house :
-        result += abs(h-i)
+    for h in ho :
+        result += abs(h-i)*count[ho]
 
     if min_value > result :
         min_value = result
--- codebook/sorting/test.py
+++ codebook/sorting/test.py
@@ -1,13 +1,18 @@
-12
-Junkyu 50 60 100
-Sangkeun 80 60 50
-Sunyoung 80 70 100
-Soong 50 60 90
-Haebin 50 60 100
-Kangsoo 60 80 100
-Donghyuk 80 60 100
-Sei 70 70 70
-Wonseob 70 70 90
-Sanghyun 70 70 80
-nsj 80 80 80
-Taewhan 50 60 90
+# 12
+# Junkyu 50 60 100
+# Sangkeun 80 60 50
+# Sunyoung 80 70 100
+# Soong 50 60 90
+# Haebin 50 60 100
+# Kangsoo 60 80 100
+# Donghyuk 80 60 100
+# Sei 70 70 70
+# Wonseob 70 70 90
+# Sanghyun 70 70 80
+# nsj 80 80 80
+# Taewhan 50 60 90
+
+
+361번 
+5	[2, 1, 2, 6, 2, 4, 3, 3]	[3,4,2,1,5]
+4	[4,4,4,4,4]	[4,1,2,3]
