72d049cc3e8093b6f270c12ea650f508866d1f19
simyeen <simyeen@naver.com>
2021-05-21 13:24:34 +0900

    360_sol

--- codebook/sorting/360.py
+++ codebook/sorting/360.py
@@ -1,23 +1,5 @@
 n = int(input())
+data = list(map(int, input().split()))
+data.sort()
 
-houses = list(map(int, input().split()))
-count = [0] * 200010
-
-for i in houses:
-    count[i] += 1
-
-house_set = set(houses) # 중복 제거 및 sort가능.
-
-result = 1e9
-answer = houses[0]
-
-for antena in house_set:
-    total = 0
-    for house in house_set:
-        total += (abs(antena - house)) * count[house]
-    
-    if result > total :
-        result = total
-        answer = antena
-        
-print(answer)
+print(data[(n-1)//2])
