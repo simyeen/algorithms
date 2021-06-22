0912b62121934f0aa4a7b4f96d2ac4a116c441b8
simyeen <simyeen@naver.com>
2021-06-22 17:43:12 +0900

    14.py

---
+++ codebook/programmers/level1/14.py
@@ -0,0 +1,17 @@
+def solution(num):
+    answer = 0
+
+    while answer != 500:
+
+        if num == 1 :
+            return answer
+
+        if num % 2 == 0 :
+            num //= 2
+        else : 
+            num = 3*num + 1
+        answer +=1 
+
+    return -1
+
+print(solution(626331))
