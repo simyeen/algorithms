a4a71bc91d051a5305ddee502603786a6fd793ff
simyeen <simyeen@naver.com>
2021-06-24 02:29:41 +0900

    10.py fail using combination

--- codebook/programmers/level1/10_re.py
+++ codebook/programmers/level1/10_re.py
@@ -1,5 +1,14 @@
+from itertools import combinations
+
 def solution(d, budget):
-    answer = 0
+    answer = []
+
+    for i in range(1,len(d)+1):
+        data = list(combinations(d, i))
+        for c in data:
+            if sum(c) == budget:
+                answer.append(i)
+    answer = max(answer)
     return answer
 
-print(solution([1,3,2,5,4]	, 9))
+print(solution([1,3,2,5,4], 9))
