aead7d5e37aca4179ae9871be97f79c9b1e8de0f
simyeen <simyeen@naver.com>
2021-06-24 02:39:41 +0900

    10_re.py

--- codebook/programmers/level1/10_re.py
+++ codebook/programmers/level1/10_re.py
@@ -1,14 +1,14 @@
 from itertools import combinations
 
 def solution(d, budget):
-    answer = []
+    answer = 0
+    d = sorted(d)
+    
+    for i in d:
+        if budget - i >= 0:
+            budget -= i
+            answer += 1 
 
-    for i in range(1,len(d)+1):
-        data = list(combinations(d, i))
-        for c in data:
-            if sum(c) == budget:
-                answer.append(i)
-    answer = max(answer)
     return answer
 
 print(solution([1,3,2,5,4], 9))
