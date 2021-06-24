909d2a25d59ab71005b756f4edcad006ec38a36d
simyeen <simyeen@naver.com>
2021-06-24 13:32:03 +0900

    1.py using re

--- codebook/programmers/level1/35.py
+++ codebook/programmers/level1/35.py
@@ -1,29 +1,24 @@
 def solution(dartResult):
     answer = 0
-    d = dartResult
-    for i in range(len(d)):
-        
-        if d[i]== '0' : continue
-
+    d = dartResult    
+    for i in range(len(d)):       
         if d[i].isdigit() :
             if d[i+1].isdigit():
                 tmp = 10
-            tmp = int(d[i])
-            continue
-
-        elif d[i] == 'S':
-            answer += tmp
+                d = d.replace('0','K')
+            else : tmp = int(d[i]) 
         elif d[i] == 'D':
             tmp **= 2
-            answer += tmp
         elif d[i] == 'T':
             tmp **= 3
-            answer += tmp
         elif d[i] == '*':
             answer *= 2
         elif d[i] == '#':
-            answer = -answer
-        print(answer)
+            answer -= tmp
+
+        answer += tmp
+        print(d[i], tmp, answer)
+    print(d)
     return answer
 
 print(solution("1D2S#10S"))
