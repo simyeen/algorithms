20fc3f3bfffc982aa4497f0a17915a6a205e7393
simyeen <simyeen@naver.com>
2021-07-09 22:43:01 +0900

    24.py

---
+++ codebook/programmers/level2/25.py
@@ -0,0 +1,8 @@
+
+
+def solution(relation): # 후보키 문제
+    answer = 0
+    return answer
+
+relation = [["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]
+print(solution(relation))
