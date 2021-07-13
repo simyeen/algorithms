da44a220ffdadc54e8fc65fe88ed6b06f465dc2c
simyeen <simyeen@naver.com>
2021-07-13 16:48:26 +0900

    17.py

--- codebook/programmers/level2/17.py
+++ codebook/programmers/level2/17.py
@@ -1,23 +1,25 @@
-from itertools import combinations
+from itertools import permutations
 
 def solution(numbers):
-    answer = 0
-    
-    num = list(numbers)
-    
-    for i in range(1,len(num)+1):        
-        combi = list(combinations(num,i)) # 3C2
-        for c in combi: # 3개중 하나의 조합
-            
-            tmp = ''
-            for i in c: # 조합
-                tmp += i
-            
-            target = int(tmp)
-            print(target)
-        
- 
-    
-    return answer
 
-print(solution("011"))
+    num = list(numbers)
+    target = set()
+    for i in range(1, len(num)+1):
+        mylist = set(list(permutations(num,i)))
+
+        for per in mylist:
+            tmp = ''
+            for i in per :
+                tmp += i
+            target.add(int(tmp))
+
+    cnt = 0     
+    for i in target:
+        if i < 2 : continue
+        for j in range(2, int(i**(1/2))):
+            if i % j == 0 : break
+        else : cnt += 1        
+
+    return cnt
+
+print(solution("013"))
