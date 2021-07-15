ad3c6750d1c53468724587340491e7cdce663efc
simyeen <simyeen@naver.com>
2021-07-15 23:18:23 +0900

    17.py succes

--- codebook/programmers/level2/17.py
+++ codebook/programmers/level2/17.py
@@ -6,17 +6,16 @@ def solution(numbers):
     target = set()
     for i in range(1, len(num)+1):
         mylist = set(list(permutations(num,i)))
-
         for per in mylist:
             tmp = ''
             for i in per :
                 tmp += i
             target.add(int(tmp))
-
     cnt = 0     
     for i in target:
-        if i < 2 : continue
-        for j in range(2, int(i**(1/2))):
+        if i < 2 : 
+            continue
+        for j in range(2, int(i**(1/2))+1):
             if i % j == 0 : break
         else : cnt += 1        
 
