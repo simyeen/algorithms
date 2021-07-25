bdf07ce983b62154d97b95f9addf49aa6076f6a4
simyeen <simyeen@naver.com>
2021-07-25 14:48:30 +0900

    15.py fail

--- codebook/programmers/level2/15.py
+++ codebook/programmers/level2/15.py
@@ -1,16 +1,30 @@
 def solution(prices):
-    answer = []
-    
-    for i in range(len(prices)-1):
-        tmp = 0
-        for j in range(i+1, len(prices)):
-            if prices[i] <= prices[j]:
-                tmp += 1
-        answer.append(tmp)
-        
-    answer.append(0)
-        
-    print(int('10',2))
-    return answer
+    answer = [0 for _ in prices]
 
-print(solution([1, 2, 3, 2, 3]))
+    for i in range(len(prices)-1):
+        down = False
+        for j in range(i+1, len(prices)):
+            if prices[i] > prices[j] : # 하락장 일때
+                if down == False : answer[i] += 1
+                down = True
+            elif prices[i] == prices[j] and down == False:
+                answer[i] += 1
+            elif prices[i] < prices[j] :
+                down = False
+                answer[i] += 1 
+                
+    return answer
+    
+
+print(solution([1, 2, 3, 2, 3, 1]))
+
+# 한참 헤매다가 다른분 설명보고이해 했네요.
+
+# prices = {1, 2, 3, 2, 3, 1} return {5, 4, 1, 2, 1, 0}
+
+# 이 테스트케이스가 왜 저렇게 떨어지는지 한 번 따라가보시고 해보시면 금방 이해하실꺼에요~
+
+# 테스트케이스 입출력 설명 예 중
+
+# 3초 시점의 ₩3은 1초뒤에 가격이 떨어집니다. 따라서 1초간 가격이 떨어지지 않은 것으로 봅니다.
+
--- codebook/programmers/level2/25.py
+++ codebook/programmers/level2/25.py
@@ -5,32 +5,38 @@ def solution(relation):
     cur = 1 
     record = [i for i in range(len(relation[0]))]
     n = len(relation)
-    check_list = []
+    check_set = set()
     
     while cur <= len(record): 
         
         combination = list(combinations(record,cur)) # 1에서 최대 n까지의 조합이다.
-
+        tmp_list = []
         for combi in combination: # 4C2 같은 조합 중에 하나    
-            for c in combi : # 이미 키인 열인지 검사
-                if c in check_list: 
-                    break
+            
+            tmp = ''
+            for c in combi: tmp += str(c)
+            for check in check_set: # 최소성 검사
+                if check in tmp: break
+            
             else :  
                 cmp_set = set()
                 for i in range(n):
                     tmp = ''
                     for c in combi: # 1, 2 전부다 더해주기
-                        tmp += relation[i][c]
+                        tmp += str(relation[i][c])
                     cmp_set.add(tmp)
+
                 if len(cmp_set) == n : # 중복없으므로 키 값으로 인정된다.
-                    for c in combi:
-                        check_list.append(c)
+                    tmp = ''
+                    for c in combi: tmp += str(c)
+                    tmp_list.append(tmp) # 해당 cur일때  
                     answer += 1
-                            
+        for check in tmp_list: check_set.add(check)
         cur += 1
     
     return answer
 
-relation = [["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","1"],["500","muzi","music","3"],["600","apeach","music","2"]]
+relations = [["100","ryan","music","2"],["200","apeach","math","4"],["300","tube","computer","3"],["400","con","computer","1"],["500","muzi","music","3"],["600","apeach","music","2"]]
+relation = [['a',1,'aaa','c','ng'],['b',1,'bbb','c','g'],['c',1,'aaa','d','ng'],['d',2,'bbb','d','ng']]
 print(solution(relation))
 
