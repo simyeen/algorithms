1de957c9d70eef613fc8c3c6c478893deef8731c
simyeen <simyeen@naver.com>
2021-07-27 02:36:46 +0900

    truck success

--- codebook/programmers/level2/24.py
+++ codebook/programmers/level2/24.py
@@ -1,31 +1,34 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    
-    time = bridge_length
-    wait = deque()
-    for i in truck_weights:
-        wait.append(i)
-
     cnt = 0
-    queue = deque()
-    while wait:
-        if len(wait) != 0 :
-            cur = wait.popleft()
+    size = bridge_length
 
-        queue.append(cur)
-        if sum(queue) <= weight: # 스택에 최대한 꽉 담기
+    start_list = deque(truck_weights)
+    bridge_list = deque()
+    end_list = deque()
+
+    while len(end_list) != len(truck_weights):
+        if len(bridge_list)==0 and start_list :
+            bridge_list.append((start_list.popleft(),cnt))
+            cnt += 1
             continue
-        wait.append(cur) # 마지막에 못 들어간 차량 다시 넣어주기
 
-        queue.popleft() # 빠져나가는 차량 발생.    
-        cnt += time
+        if bridge_list: # 나가는 트럭 검사
+            if bridge_list[0][1]+size == cnt: 
+                truck, trash = bridge_list.popleft()
+                end_list.append(truck)
 
-        
+        if start_list: # 들어오는 트럭 검사
+            truck = start_list.popleft()
 
+            sum_value = 0
+            for value, time in bridge_list: sum_value += value
 
+            if sum_value + truck <= weight:
+                bridge_list.append((truck,cnt))
+            else : start_list.appendleft(truck)
+
+        cnt += 1
 
     return cnt
-
-
-print(solution(2,10,[7,4,5,6]))
