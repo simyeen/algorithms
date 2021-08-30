16dd17d2e8ae6e97dbe84b594cc0b1a7fb2acd13
simyeen <simyeen@naver.com>
2021-08-31 02:23:24 +0900

    async testing

---
+++ codebook/JS/json.js
@@ -0,0 +1,5 @@
+// 1. 오브젝트를 제이슨으로 변화시키기
+
+JSON.stringify(obj); // 제이슨이 문자열이니까 스트링 시키기!
+
+const json = JSON.stringify(["apple", "bananan"]);
--- codebook/JS/sorting.js
+++ codebook/JS/sorting.js
@@ -1,5 +1,6 @@
-const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+// insert sorting
 
+const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 let min_index = 1
 for(let i = 0; i < n){
