1cf8502848051a2fc15c6d39b9dcad2630c34b06
simyeen <simyeen@naver.com>
2021-09-17 03:36:15 +0900

    17142.py

--- codebook/samsung/17142.py
+++ codebook/samsung/17142.py
@@ -1,64 +1,79 @@
-
 from itertools import combinations
 from collections import deque
-import copy
+
+n, m = map(int, input().split())
+data, viruses = [], []
+for _ in range(n) : data.append(list(map(int, input().split())))
 
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 
-n, m = map(int, input().split())
-data = []
-for _ in range(n) : data.append(list(map(int, input().split())))
-
-check = 0
-blocks, virus = [], []
-dist_data = [[' ' for _ in range(n)] for _ in range(n)]
+original_arr = [['' for _ in range(n)] for _ in range(n)]
 for i in range(n):
     for j in range(n):
-        if data[i][j] == 2 : 
-            virus.append((i,j))
-            dist_data[i][j] = '*'
-        elif data[i][j] == 1 : 
-            blocks.append((i,j))
-            dist_data[i][j] = '-'
-        else : check += 1
-check += len(virus) # 바이러스와 빈칸의 합.
+        if data[i][j] == 1 : original_arr[i][j] = '-'
+        elif data[i][j] == 0 : original_arr[i][j] = 'O'
+        else : 
+            original_arr[i][j] = '*'
+            viruses.append((i,j)) 
+
+combination = list(combinations(viruses, m))
 
 ans = []
-combination = list(combinations(virus, m))
-for combi in combination:
+for combi in combination:    
+    max_value = -1e9
+    arr = [item[:] for item in original_arr]
     q = deque()
-    tmp_data = copy.deepcopy(dist_data)
-
-    for virus in combi : 
+    for virus in combi:
         x, y = virus
-        dist = 0
+        arr[x][y] = 0
         q.append((x,y,0))
-        tmp_data[x][y] = 0 # 활성 바이러스들
 
-    cnt, max_value = 0, -1e9
+    passive_virus = []
     while q:
         x, y, dist = q.popleft()
-        cnt += 1 # 우선적으로 바이러스만큼 찬다. 
-
-        if tmp_data[x][y] != '+' : # 비활성 바이러스를 만나면은?
-            tmp_data[x][y] = str(dist)
-            max_value = max(dist, max_value)
-            
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            if not (0<=nx<n and 0<=ny<n) : continue
-            if tmp_data[nx][ny] == '-' : continue
-
-            if tmp_data[nx][ny] == '*' : # 비활성 바이러스를 만났을 때
-                tmp_data[nx][ny] = '+' # 활성으로 변해버린 바이러스
+            if not(0<=nx<n and 0<=ny<n) : continue
+            if arr[nx][ny] == '-' : continue
+            if arr[nx][ny] == 'O' or arr[nx][ny] == '*': 
+                arr[nx][ny] = dist+1
                 q.append((nx,ny,dist+1))
-                continue
-            if tmp_data[nx][ny] == ' ':
-                tmp_data[nx][ny] = str(dist+1)
-                q.append((nx,ny,dist+1))
-    if check == cnt : ans.append(max_value)
-    else : ans.append(-1)
+                max_value = max(max_value, dist+1)
 
-if max(ans) == -1 : print(-1)
-else : print(min(ans))
+    max_value = 0
+    for i in range(n):
+        for j in range(n):
+            if (i,j) in viruses : arr[i][j] = '*'
+            if str(type(arr[i][j])) == "<class 'int'>":
+                max_value = max(arr[i][j], max_value)
+
+    for item in arr : # 꽉 채우기 실패
+        if 'O' in item : 
+            max_value = -1
+
+    ans.append(max_value)
+    
+
+if -len(ans) == sum(ans): print(-1)
+else : 
+    answer = 1e9
+    for an in ans : 
+        if an == -1 : continue
+        answer = min(answer, an)
+    print(answer)
+
+
+
+# 1. virus m개를 뽑고 활성 바이러스로 본다.
+# 2. 활성바이러스를 넣고 bfs시킨다.
+# 3. 비활성바이러스는 활성바이러스를 만나면 활성화 된다.
+# 4. 
+
+
+
+
+
+
+
+
