7aa8980411d788d8507a527447e24c4a568ff84f
simyeen <simyeen@naver.com>
2021-09-17 20:40:45 +0900

    17779.py

--- codebook/samsung/17779.py
+++ codebook/samsung/17779.py
@@ -1,52 +1,60 @@
+# 게리멘더링2 => 2시간 15분...
+
 from itertools import product
-import copy
 
 n = int(input())
 data = []
-for _ in range(n) : data.append(list(map(int,input().split())))
+for _ in range(n) : data.append(list(map(int, input().split())))
 
-# x,y,d1,d2를 어떻게 정할까?
+d1_list = [i+1 for i in range(int(n/2))]
+d2_list = [i+1 for i in range(int(n/2))]
+d_combination = list(product(d1_list, d2_list))
 
-point_list = [i for i in range(n)]
-d_list = [i for i in range(1,n)]
-
-products = list(product(point_list,point_list,d_list,d_list))
-
-combinations = []
-for pro in products:
-    x,y,d1,d2 = pro 
-    if x < x+d1+d2 <= n-1 :
-        if 0 <= y-d1 < y and y < y+d2 <= n-1:
-            combinations.append(pro)
-
-def count():pass
+ans = []
+for combination in d_combination:
+    d1, d2 = combination
+    five_list = []
+    for i in range(n):
+        for j in range(n):
+            if 0<= i + d1 + d2 <= n-1:
+                if 0<= j-d1 < j and j < j+d2 <= n-1 : five_list.append((i,j))
     
-for combi in combinations:
-    x,y,d1,d2 = combi
-    
-    state_data = [[0 for _ in range(n)] for _ in range(n)]
-    state_data[x][y] = 6
-    state_data[x+d1][y-d1] = 7
-    state_data[x+d2][y+d2] = 7
-    state_data[x+d2+d1][y+d2-d1]=7
+    arr = [[0 for _ in range(n)] for _ in range(n)]
+    for x,y in five_list:
+        edges = set()
 
-    print('전')
-    for i in state_data : print(i)
-    print()
+        for i in range(d1+1): edges.add((x+i,y-i))
+        for i in range(d2+1): edges.add((x+i,y+i))
+        for i in range(d2+1): edges.add((x+d1+i,y-d1+i))
+        for i in range(d1+1): edges.add((x+d2+i,y+d2-i))
 
-    for r in range(n):
-        for c in range(n):
-            if 0 <= r < x+d1-1 and 0<= c <= y : state_data[r][c] = 1
-            elif 0 <= r <= x+d2 and y-1 < c <= n-1 : state_data[r][c] = 2
-            elif x+d1-1 <= r <= n-1 and 0 <= c < y-d1+d2: state_data[r][c] = 3
-            elif x+d2 < r <= n-1 and y-d1+d2-1 <= c <= n-1: state_data[r][c] = 4
+        flag = False
+        for i in range(n):
+            for j in range(n):
+                if (i,j) == (x,y) or (i,j) == (x+d1+d2, y+d2-d1):
+                    arr[i][j] = 5
+                    continue
+                if (i,j) in edges :
+                    if flag == False: flag = True                        
+                    else : flag = False
+                    arr[i][j] = 5
+                    continue
+                if flag == True : 
+                    arr[i][j] = 5
+                    continue
+        
+                if 0<=i<x+d1 and 0<=j<=y : arr[i][j] = 1
+                elif 0<=i<=x+d2 and y<j<=n-1 : arr[i][j] = 2
+                elif x+d1<=i<=n-1 and 0<=j<y-d1+d2 : arr[i][j] = 3
+                elif x+d2<i<=n-1 and y-d1+d2<=j<=n-1 : arr[i][j] = 4
 
-    state_data[x][y] = 5
-    state_data[x+d1][y-d1] = 5
-    state_data[x+d2][y+d2] = 5
-    state_data[x+d2+d1][y+d2-d1]=5
-    print('후')
-    for i in state_data:
-        print(i)
-    print()
+        tmp_list = [0 for _ in range(6)]
+        for i in range(n):
+            for j in range(n):
+                tmp_list[arr[i][j]] += data[i][j]
+        
+        tmp_list = tmp_list[1:]
+        ans.append(max(tmp_list) - min(tmp_list))
+
+print(min(ans))
 
