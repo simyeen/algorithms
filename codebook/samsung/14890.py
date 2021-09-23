89776b2374590182a4759e72e8bd2f4c10644718
simyeen <simyeen@naver.com>
2021-09-24 02:05:33 +0900

    14890.py

--- codebook/samsung/14890.py
+++ codebook/samsung/14890.py
@@ -1,39 +1,54 @@
-n, l = map(int, input().split())
+# 경사로 문제
 
-data = []
-for _ in range(n): data.append(list(map(int, input().split())))
-
-# 행 순으로 돌기
-ans = 0
-
-for i in range(n):
-    j = 0
-    while j < n-l:
-        target1, cnt = data[i][j], 0
-        target2 = data[i][j+1]
-        check = False
-
-        if abs(target1-target2) >= 2 : break
-        for k in range(l-1):
-            if target2 != data[i][j+k] : 
-                check = True
-                break
+n, k = map(int, input().split())
 
 
+def check_cnt(arr, index, case, flag):
+    global k
 
-        j += l
+    if case == 0 : 
+        target = arr[index]
+        for i in range(index, index-k, -1):
+            if not (0<=i<n) or flag[i]: return False
+            if target != arr[i] : return False
+        for i in range(index, index-k, -1): flag[i] = True            
+        return True
+    else : 
+        target = arr[index+1]
+        for i in range(index+1, index+k+1):
+            if not (0<=i<n) or flag[i]: return False
+            if target != arr[i] : return False
+        for i in range(index+1, index+k+1): flag[i] = True
+        return True
 
+def check_path(arr):
+    global n
 
-    print()
+    flag = [False for _ in range(n)]
+    i = 0
+    while i < n-1:
+        if arr[i] == arr[i+1]: 
+            i += 1
+            continue    
+        if arr[i]+1 == arr[i+1] : # 다음 구간이 1만큼 클때
+            if check_cnt(arr, i, 0, flag) == False : return False
+            i += 1
+            continue
+        elif arr[i]-1 == arr[i+1] :
+            if check_cnt(arr, i, 1, flag) == False : return False
+            i += 1
+            continue
+        else : return False
+    return True
 
-# 열 순으로 돌기
-for j in range(n):
-    for i in range(n):
-        pass
+data, ans = [], 0
+for _ in range(n) : data.append(list(map(int, input().split())))
+for row in data: # row 검사
+    if check_path(row) == True : ans += 1
 
+for j in range(n): # col 검사
+    col = []
+    for i in range(n): col.append(data[i][j])
+    if check_path(col) == True: ans +=1
+print(ans)
 
-# 경사로를 어떤 순으로 놓을까?
-# 행 / 열 순서로 놓기
-# 행에서도 왼 / 오 이렇게 2번 돌릴까?
-# 가다가 높은 애 만나면 이전꺼 L개 확인하기
-# 가다가 낮은 애 만나면 다음꺼 L개 확인하기
